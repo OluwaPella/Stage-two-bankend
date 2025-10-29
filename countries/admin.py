ca59fa3f08e58d031583fa9f1723774674eae161
Olaosebikan pelumi <oolaosebikan360@gmail.com>
2025-10-29 22:59:05 +0100

    added: code update

--- countries/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Country
-
-@admin.register(Country)
-class CountryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'capital', 'region', 'population', 'currency_code', 'estimated_gdp']
-    list_filter = ['region', 'currency_code']
-    search_fields = ['name', 'capital']
